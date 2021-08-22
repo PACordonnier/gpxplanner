b6a7a7ffc7349f040724b5d1d46cd945c7221151
Paul-Adrien Cordonnier <pauladrien@adaltas.com>
2021-08-22 20:30:38 +0200

    Add insert into couchbase

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,7 @@
 const Hapi = require('@hapi/hapi');
 const fs = require('fs');
 const Joi = require('joi')
+const nano = require('nano')('http://admin:password@localhost:5984');
 
 const init = async () => {
 
@@ -17,14 +18,22 @@ const init = async () => {
     path: '/upload',
     config: {
       handler: (request, h) => {
-          let writeStream = fs.createWriteStream('db/route.gpx');
 
           const payload = request.payload;
-          console.log(payload);
+          let writeStream = fs.createWriteStream("db/" + payload.name + ".gpx");
 
-          payload.upload.pipe(writeStream)
-          
-          return new Promise(resolve => writeStream.on('finish', () => writeStream.close(() => resolve("Upload successful !"))))
+          payload.upload.pipe(writeStream);
+          const gpxplanner = nano.use('gpxplanner');
+          // nano.db.create('gpxplanner')
+          // const gpxplanner = nano.use('gpxplanner')
+          // const response = await gpxplanner.insert({ path: "db/" + payload.name + ".gpx", name: payload.name, date: payload.date })
+          let promises = new Promise(
+            resolve => writeStream.on('finish', () => writeStream.close(() => resolve("Upload successful !")))
+          ).then(gpxplanner.insert({ path: "db/" + payload.name + ".gpx", name: payload.name, date: payload.date }))
+
+      
+
+          return promises
         },
 
       payload: {
@@ -33,13 +42,14 @@ const init = async () => {
         parse: true,
         multipart: true
       },
-      
+
       validate: {
         payload: Joi.object({
-            name: Joi.string().required(),
-            upload: Joi.required()
+            name: Joi.string().alphanum().required(),
+            upload: Joi.required(),
+            date: Joi.date().required()
         })
-    }
+     }
 
     }
   });
