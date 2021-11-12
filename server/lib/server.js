f752e8f9c2142c971da71ba9af8f599ca7aae33f
Paul-Adrien Cordonnier <pauladrien@adaltas.com>
2021-11-12 23:14:18 +0100

    server: delete query endpoint

--- server/lib/server.js
+++ server/lib/server.js
@@ -23,38 +23,6 @@ const server = Hapi.server({
 })
 
 server.route({
-  method: 'GET',
-  path: '/query',
-  config: {
-    handler: async (request, h) => {
-      var selector = {}
-      Object.keys(request.query).forEach((key) => {
-        selector[key] = { "$eq" : request.query[key] instanceof Date ? request.query[key].getTime()/1000 : request.query[key]};
-      });
-      var q = {
-        "selector": selector
-      };
-      try {
-        const response = await routes_db.find(q);
-        return h.response(response.docs);
-      }
-      catch (error) {
-        console.log(error);
-        throw error
-      }
-      
-    },
-
-    validate: {
-      query: Joi.object({
-        date: Joi.date().iso(),
-        name: Joi.string()
-      })
-    }
-  }
-})
-
-server.route({
   method: 'POST',
   path: '/routes',
   config: {
